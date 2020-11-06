f7046781fac3db8952ffa43e412e68f63d606f50
Allen Huang <ligerx066@gmail.com>
2020-11-05 16:08:23 -0800

    added css rules for each direction and removed inline style

--- main.js
+++ main.js
@@ -1,25 +1,31 @@
 
 var $car = document.querySelector('.car');
 
-var directions = ['left', 'right', 'up', 'down'];
-var currentDirection = 0;
+var direction = 0;
 
 document.addEventListener('keydown', function(e) {
-  // console.log('keycode: ', e.keyCode);
-  // console.log('code: ', e.code);
 
   if(e.code === 'KeyA') {
-    currentDirection -= 45;
+    direction--;
 
-    $car.setAttribute('style', ' width: 100px; transform-origin: center; transform: rotate(' +
-      currentDirection + 'deg);');
+    if(direction < 0) {
+      direction = 8 - (-1*direction);
+    }
+
+    $car.className = 'car direction' + direction;
+
   } else if (e.code === 'KeyD') {
-    currentDirection += 45;
-    $car.setAttribute('style', ' width: 100px; transform-origin: center; transform: rotate(' +
-      currentDirection + 'deg);');
-  } else if (e.code === 'KeyS') {     //s
+    direction++;
 
-  } else if (e.code === 'KeyW') {     //w
+    if (direction > 7) {
+      direction = 0;
+    }
+
+    $car.className = 'car direction' + direction;
+
+  } else if (e.code === 'KeyS') {
+
+  } else if (e.code === 'KeyW') {
 
   }
 
